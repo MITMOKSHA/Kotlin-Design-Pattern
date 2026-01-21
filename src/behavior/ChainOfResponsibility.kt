8a47309d805e80160bb4d803e23ab62fdf9b0d7c
MITMOKSHA <490135851@qq.com>
2026-01-21 23:30:37 +0800

    完成责任链模式

--- src/behavior/ChainOfResponsibility.kt
+++ src/behavior/ChainOfResponsibility.kt
@@ -1,4 +1,47 @@
 package behavior
 
-class ChainOfResponsibility {
+abstract class Handler(
+    protected val next: Handler?
+) {
+    abstract fun canHandle(request: Request): Boolean
+    fun handle(request: Request) {
+        if (!canHandle(request)) {
+            next?.handle(request)
+        }
+    }
+}
+
+class CEO(handler: Handler? = null): Handler(handler) {
+    override fun canHandle(request: Request): Boolean {
+        println("CEO审批")
+        return true
+    }
+}
+
+class Boss(handler: Handler? = null): Handler(handler) {
+    override fun canHandle(request: Request): Boolean {
+        println("部门主管审批")
+        return request.days in 3..5
+    }
+}
+
+class Manager(handler: Handler? = null): Handler(handler) {
+    override fun canHandle(request: Request): Boolean {
+        println("经理审批")
+        return request.days < 3
+    }
+}
+
+
+class Request(
+    val days: Int
+)
+
+fun main() {
+    val ceo = CEO()
+    val boss = Boss(ceo)
+    val manager = Manager(boss)
+    // 员工请假
+    val request = Request(7)
+    manager.handle(request)
 }
