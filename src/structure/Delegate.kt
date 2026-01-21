b0122135667a61cb6c341c21fdef063ddad53f98
MITMOKSHA <490135851@qq.com>
2026-01-21 23:58:15 +0800

    完成代理模式

--- src/structure/Delegate.kt
+++ src/structure/Delegate.kt
@@ -1,4 +1,27 @@
 package structure
 
-class Delegate {
+interface Leader {
+    fun signDocument(name: String) // 签署文件
+}
+
+// 老板(委托者)
+class Boss: Leader {
+    override fun signDocument(name: String) {
+       println("老板签署了文件：$name")
+    }
+}
+
+// 秘书(代理人)
+class Secret(
+    private val boss: Boss,
+): Leader {
+    override fun signDocument(name: String) {
+        println("秘书收到文件: $name")
+        boss.signDocument(name)
+    }
+}
+
+fun main() {
+    val secret = Secret(Boss())
+    secret.signDocument("项目合同")
 }
