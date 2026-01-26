2fa571d97a6e2570d6688a70eab2911b7c7f85cc
MITMOKSHA <490135851@qq.com>
2026-01-27 00:05:30 +0800

    完成状态模式

--- src/behavior/State.kt
+++ src/behavior/State.kt
@@ -1,4 +1,53 @@
 package behavior
 
-class State {
+interface State {
+    fun handle(context: Context)
+}
+
+class InitState : State {
+    override fun handle(context: Context) {
+        println("onDestroy -> onInit")
+        context.setState(StartState())
+    }
+}
+
+class StartState : State {
+    override fun handle(context: Context) {
+        println("onInit -> onStart")
+        context.setState(StopState())
+    }
+}
+
+class StopState : State {
+    override fun handle(context: Context) {
+        println("onStart -> onStop")
+        context.setState(DestroyState())
+    }
+}
+
+class DestroyState : State {
+    override fun handle(context: Context) {
+        println("onStop -> onDestroy")
+        context.setState(InitState())
+    }
+}
+
+class Context(
+    private var currentState: State
+) {
+    fun request() {
+        currentState.handle(this)
+    }
+
+    fun setState(state: State) {
+        currentState = state
+    }
+}
+
+fun main() {
+    val context = Context(StartState())
+    context.request()
+    context.request()
+    context.request()
+    context.request()
 }
