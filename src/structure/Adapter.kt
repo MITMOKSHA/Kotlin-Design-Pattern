5a3336157239454f0f5dc90534c5a836cbd4b731
MITMOKSHA <490135851@qq.com>
2026-01-27 00:17:27 +0800

    完成适配器模式

--- src/structure/Adapter.kt
+++ src/structure/Adapter.kt
@@ -1,4 +1,25 @@
 package structure
 
-class Adapter {
+interface TypeCCharger {
+    fun chargingWithTypeC()
+}
+
+class TypeCChargerAdapter(
+    private val lightningCharger: LightningCharger
+): TypeCCharger {
+    override fun chargingWithTypeC() {
+        lightningCharger.chargingWithLightning()
+        println("适配器：${lightningCharger.chargingWithLightning()} 转换为 type-c")
+    }
+}
+
+class LightningCharger {
+    fun chargingWithLightning(): String {
+        return "使用Lightning充电"
+    }
+}
+
+fun main() {
+    val typeCharger = TypeCChargerAdapter(LightningCharger())
+    typeCharger.chargingWithTypeC()
 }
