639fa4f434808cd5065169469456dcbe3e182389
MITMOKSHA <490135851@qq.com>
2026-01-26 23:48:49 +0800

    完成组合模式

--- src/structure/Composition.kt
+++ src/structure/Composition.kt
@@ -1,4 +1,48 @@
 package structure
 
-class Composition {
+interface Component {
+    fun display()
+}
+
+class Composite: Component {
+    private val children: MutableList<Component> = mutableListOf()
+    override fun display() {
+        children.forEach { it.display() }
+    }
+
+    fun addChild(component: Component) {
+        children.add(component)
+    }
+
+    fun removeChild(component: Component) {
+        children.remove(component)
+    }
+
+}
+
+class Leaf(
+    private val name: String,
+): Component {
+    override fun display() {
+        println(name)
+    }
+}
+
+fun main() {
+    val leaf1 = Leaf("节点1")
+    val leaf2 = Leaf("节点2")
+    val leaf3 = Leaf("节点3")
+    val leaf4 = Leaf("节点4")
+    val leaf5 = Leaf("节点5")
+    val composite = Composite().apply {
+        addChild(leaf1)
+        addChild(leaf2)
+        addChild(leaf3)
+        addChild(leaf4)
+    }
+    val composite2 = Composite().apply {
+        addChild(composite)
+        addChild(leaf5)
+    }
+    composite2.display()
 }
