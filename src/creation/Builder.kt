b6fa5a2f2f07493c5fa8745e633b352bcc6eb111
MITMOKSHA <490135851@qq.com>
2026-01-26 23:12:03 +0800

    完成建造者、装饰器模式

--- src/creation/Builder.kt
+++ src/creation/Builder.kt
@@ -1,4 +1,45 @@
 package creation
 
-class Builder {
+class Computer(
+    private val cpu: String?,
+    private val ram: String?,
+    private val ssd: String?,
+    private val disk: String?
+) {
+    class Builder {
+        private var cpu: String? = null
+        private var ram: String? = null
+        private var ssd: String? = null
+        private var disk: String? = null
+
+        fun cpu(cpu: String): Builder {  // 可以用apply简化
+            this.cpu = cpu
+            return this
+        }
+
+        fun ram(ram: String) = apply {
+            this.ram = ram
+        }
+
+        fun ssd(ssd: String) = apply {
+            this.ssd = ssd
+        }
+
+        fun disk(disk: String) = apply {
+            this.disk = disk
+        }
+
+        fun build(): Computer {
+            return Computer(cpu, ram, ssd, disk)
+        }
+    }
+
+    override fun toString(): String {
+        return this.cpu + " " + this.ram + " " + this.ssd + " " + this.disk
+    }
+}
+
+fun main() {
+    val computer = Computer.Builder().cpu("英特尔").ram("金士顿").ssd("ssd").disk("disk").build()
+    println(computer)
 }
--- src/structure/Decorate.kt
+++ src/structure/Decorate.kt
@@ -1,4 +1,49 @@
 package structure
 
-class Decorate {
+interface Coffee {
+    fun cost(): Double
+    fun description(): String
+}
+
+class SimpleCoffee: Coffee {
+    override fun cost(): Double = 1.0
+    override fun description(): String = "普通咖啡"
+}
+
+abstract class CoffeeDecorator(
+    protected val coffee: Coffee,
+): Coffee {
+    override fun cost(): Double = coffee.cost()
+    override fun description(): String = coffee.description()
+}
+
+class MilkDecorator(
+    coffee: Coffee,
+): CoffeeDecorator(coffee) {
+    override fun cost(): Double {
+        return coffee.cost() + 3.0
+    }
+    override fun description(): String  {
+        return coffee.description() + "(加牛奶)"
+    }
+}
+
+class SugarDecorator(
+    coffee: Coffee,
+): CoffeeDecorator(coffee) {
+    override fun cost(): Double {
+        return super.cost() + 2.0
+    }
+
+    override fun description(): String {
+        return coffee.description() + "(加糖)"
+    }
+}
+
+fun main() {
+    val coffee = SimpleCoffee()
+    val milkCoffee = MilkDecorator(coffee)
+    val milkSugarCoffee = SugarDecorator(milkCoffee)
+    println("${milkCoffee.description()} ${milkCoffee.cost()}")
+    println("${milkSugarCoffee.description()} ${milkSugarCoffee.cost()}")
 }
