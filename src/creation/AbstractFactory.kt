a0c077a7d576e3bae58e26dec01ab52282957e10
MITMOKSHA <490135851@qq.com>
2026-01-28 23:48:16 +0800

    完成抽象工厂模式

--- src/creation/AbstractFactory.kt
+++ src/creation/AbstractFactory.kt
@@ -1,4 +1,64 @@
 package creation
 
-abstract class AbstractFactory {
+abstract class ComputerFactory {
+    abstract fun createRam(): Ram
+    abstract fun createCpu(): Cpu
+}
+
+class WindowsFactory: ComputerFactory() {
+    override fun createRam(): Ram {
+        return KinstonRam()
+    }
+
+    override fun createCpu(): Cpu {
+        return IntelCpu()
+    }
+}
+
+class MacFactory: ComputerFactory() {
+    override fun createRam(): Ram {
+        return SumsungRam()
+    }
+
+    override fun createCpu(): Cpu {
+        return AMDCpu()
+    }
+}
+
+interface Ram {
+}
+
+class KinstonRam: Ram {
+    init {
+        println("KinstonRam")
+    }
+}
+
+class SumsungRam: Ram {
+    init {
+        println("SuamsungRam")
+    }
+}
+
+interface Cpu
+
+class IntelCpu: Cpu {
+    init {
+        println("IntelCpu")
+    }
+}
+
+class AMDCpu: Cpu {
+    init {
+        println("AMDCpu")
+    }
+}
+
+fun main() {
+    val factory = WindowsFactory()
+    factory.createCpu()
+    factory.createRam()
+    val factory2 = MacFactory()
+    factory2.createCpu()
+    factory2.createRam()
 }
