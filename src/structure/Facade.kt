5b30ade3d67cf0a5ade7a46b084e514d68ebe026
huangpishun03 <huangpishun03@kuaishou.com>
2026-01-29 14:34:32 +0800

    完成外观模式

--- src/structure/Facade.kt
+++ src/structure/Facade.kt
@@ -1,4 +1,41 @@
 package structure
 
-class Facade {
+class Amplifier {
+    fun on() = println("功放打开")
+    fun off() = println("功放关闭")
+}
+class DVDPlayer {
+    fun play() = println("DVD播放")
+    fun stop() = println("DVD播放结束")
+}
+class Projector {
+    fun down() = println("投影仪放下")
+    fun up() = println("投影仪收起")
+}
+
+class TheaterFacade {
+    private val projector = Projector()
+    private val dvd = DVDPlayer()
+    private val amplifier = Amplifier()
+
+    fun watchMove() {
+        println("===开始播放电影")
+        projector.down()
+        amplifier.on()
+        dvd.play()
+    }
+
+    fun endMove() {
+        println("===结束播放电影")
+        dvd.stop()
+        amplifier.off()
+        projector.up()
+    }
+}
+
+fun main() {
+    val theater = TheaterFacade()
+    theater.watchMove()
+    println("===观影...")
+    theater.endMove()
 }
