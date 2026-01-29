be7bcd780549171dee93d471a745617f75bb117c
huangpishun03 <huangpishun03@kuaishou.com>
2026-01-29 13:14:18 +0800

    完成策略模式

--- src/behavior/Strategy.kt
+++ src/behavior/Strategy.kt
@@ -1,4 +1,36 @@
 package behavior
 
-class Strategy {
+interface Strategy {
+    fun forward()
+}
+
+class WalkStrategy : Strategy {
+    override fun forward() {
+        println("走路")
+    }
+}
+
+class RunStrategy : Strategy {
+    override fun forward() {
+        println("跑步")
+    }
+}
+
+class Ctx(
+    private var strategy: Strategy?
+) {
+    fun setStrategy(strategy: Strategy) {
+        this.strategy = strategy
+    }
+
+    fun exec() {
+        strategy?.forward()
+    }
+}
+
+fun main() {
+    val ctx = Ctx(WalkStrategy())
+    ctx.exec()
+    ctx.setStrategy(RunStrategy())
+    ctx.exec()
 }
